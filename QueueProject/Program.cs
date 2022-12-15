95bf7233c9c860530a15a480f97e6bc321b19c13
mago mahi <maheshhonnamurthy099@gmail.com>
2022-12-15 18:01:57 +0530

    [Mahesh]Add: Queues

--- QueueProject/Program.cs
+++ QueueProject/Program.cs
@@ -1,2 +1,30 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using QueueProject;
+using System.Collections.Generic;
+
+using System;
+
+namespace QueueProject
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                Console.WriteLine("click below options:1.Queue");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        Queues queues = new Queues();
+                        queues.Enqueue(10);
+                        queues.Enqueue(20);
+                        queues.Enqueue(30);
+                        queues.Dequeue();
+                        queues.Display();
+                        break;
+                }
+            }
+        }
+    }
+}
---
+++ QueueProject/Queues.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace QueueProject
+{
+    internal class Queues//first in first out in data structure
+    {
+        Node rear;
+        Node front;
+        public void Enqueue(int data)//Enqueue adds an element to the end of the Queue.
+        {
+            Node node = new Node(data);
+            if (front == null)
+            {
+                rear = node;
+                front = rear;
+            }
+            else
+            {
+                rear.next = node;
+                rear = rear.next;
+            }
+        }
+        public void Display()
+        {
+            if (front == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            Node temp = front;
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data);
+                temp = temp.next;
+            }
+        }
+        public void Dequeue()// removes the oldest element from the start of the Queue.
+        {
+            if (front == null)
+            {
+                Console.WriteLine("Queue is empty");
+            }
+            Console.WriteLine("{0} element dequeue ", front.data);
+            front = front.next;
+        }
+    }
+}
